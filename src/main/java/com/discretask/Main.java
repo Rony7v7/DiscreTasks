eb5740f5c7e54873ed1bd967229c1bd9ac03f4e4
JuanDLP <juanjosedelapava1@gmail.com>
2023-09-18 10:19:08 -0500

    Init commit

---
+++ src/main/java/com/discretask/Main.java
@@ -0,0 +1,16 @@
+package com.discretask;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+}
