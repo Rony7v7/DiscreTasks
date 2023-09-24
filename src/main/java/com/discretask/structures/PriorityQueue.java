ee54fb1aeb9bbcfc058f884dad7484d72da2a1e1
David Artunduaga <1040871155@u.icesi.edu.co>
2023-09-24 17:55:47 -0500

    Implementation of PriorityQueue (Heap) :)

---
+++ src/main/java/com/discretask/structures/PriorityQueue.java
@@ -0,0 +1,82 @@
+package com.discretask.structures;
+
+public class PriorityQueue<T> {
+    private Object[] heap; 
+    private int size;
+    private int capacity;
+
+    public PriorityQueue(int capacity) {
+        this.capacity = capacity;
+        this.heap = new Object[capacity];
+        this.size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void offer(T item) {
+        if (size == capacity) {
+            throw new IllegalStateException("PriorityQueue is full");
+        }
+        heap[size] = item;
+        int index = size;
+        size++;
+
+        while (index > 0) {
+            int parentIndex = (index - 1) / 2;
+            Comparable<T> currentItem = (Comparable<T>) heap[index]; 
+            Comparable<T> parentItem = (Comparable<T>) heap[parentIndex];
+
+            if (currentItem.compareTo((T) parentItem) >= 0) {
+                break;
+            }
+            swap(index, parentIndex);
+            index = parentIndex;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public T poll() {
+        if (isEmpty()) {
+            throw new IllegalStateException("PriorityQueue is empty");
+        }
+        T root = (T) heap[0];
+        size--;
+        heap[0] = heap[size];
+        int index = 0;
+
+        while (true) {
+            int leftChild = 2 * index + 1;
+            int rightChild = 2 * index + 2;
+            int smallest = index;
+
+            if (leftChild < size && ((Comparable<T>) heap[leftChild]).compareTo((T) heap[smallest]) < 0) {
+                smallest = leftChild;
+            }
+            if (rightChild < size && ((Comparable<T>) heap[rightChild]).compareTo((T) heap[smallest]) < 0) {
+                smallest = rightChild;
+            }
+            if (smallest == index) {
+                break;
+            }
+            swap(index, smallest);
+            index = smallest;
+        }
+        return root;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void swap(int i, int j) {
+        Object temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+   
+}
+
